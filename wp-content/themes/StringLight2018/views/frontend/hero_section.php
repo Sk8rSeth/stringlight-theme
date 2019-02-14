a322ae25d8ff11ba6513e226217f440304f7549b
Sk8rSeth <sethhowell2197@gmail.com>
2019-02-13 21:13:26 -0700

    hero section h2 update

--- wp-content/themes/StringLight2018/views/frontend/hero_section.php
+++ wp-content/themes/StringLight2018/views/frontend/hero_section.php
@@ -2,9 +2,15 @@
     <div class="abstract"></div>
     <div class="page-content small-24 medium-18">
         <div class="grid-x grid-container align-middle">
-            <h2 class="h1 cell small-22 medium-18"><?= $module['top_line']; ?></h2>
-            <h2 class="h1 cell small-22 small-offset-3 medium-13 medium-offset-10"><?= $module['middle_line']; ?></h2>
-            <h2 class="h1 cell small-24"><?= $module['bottom_line']; ?></h2>
+            <div class="cell small-22 medium-14 medium-offset-4">
+                <h2 class="h1"><?= $module['top_line']; ?></h2>
+            </div>
+            <div class="cell small-22 small-offset-3 medium-13 medium-offset-10">
+                <h2 class="h1"><?= $module['middle_line']; ?></h2>
+            </div>
+            <div class="cell small-24 medium-17 medium-offset-7">
+                <h2 class="h1"><?= $module['bottom_line']; ?></h2>
+            </div>
             <div class="cell small-20 small-offset-4 subheading">
                 <?= $module['subheading_copy']; ?>
             </div>
