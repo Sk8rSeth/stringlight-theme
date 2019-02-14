facd80f9f581521bfc0fc9d555734810fc8b9334
Sk8rSeth <sethhowell2197@gmail.com>
2019-02-13 21:14:00 -0700

    form and footer sections

--- wp-content/themes/StringLight2018/footer.php
+++ wp-content/themes/StringLight2018/footer.php
@@ -1,3 +1,14 @@
+    <footer>
+        <div class="grid-container grid-x align-justify align-middle footer-container">
+            <div class="small-24 medium-6 cell">
+                This is where a menu goes
+            </div>
+            <div class="small-24 medium-6 cell">
+                &copy; <?php echo date('Y'); ?> Stringlight Creative
+            </div>
+        </div>
+    </footer>
+
     <?php wp_footer(); ?>
   </body>
 </html>
---
+++ wp-content/themes/StringLight2018/views/frontend/form_section.php
@@ -0,0 +1,16 @@
+<div class="grid-container module form-section">
+    <div class="small-22 medium-8 medium-offset-4 cell">
+        <h2><?= $module['headline']; ?></h2>
+    </div>
+    <div class="small-22 medium-10 medium-offset-2">
+        <p><?= $module['subheadline']; ?></p>
+    </div>
+    <div class="small-24 cell">
+        <div class="grid-container grid-x align-justify">
+            <input type="text" class="cell small-6" placeholder="Text Input..">
+            <input type="text" class="cell small-6" placeholder="Text Input..">
+            <input type="text" class="cell small-6" placeholder="Text Input..">
+            <a class="submit btn">Submit</a>
+        </div>
+    </div>
+</div>
