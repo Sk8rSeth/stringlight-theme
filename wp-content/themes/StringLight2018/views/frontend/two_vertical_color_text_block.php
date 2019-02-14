f97ef274bda8c9d735c239a994910e578d3806bc
Sk8rSeth <sethhowell2197@gmail.com>
2019-02-13 21:13:08 -0700

    two column color text block

---
+++ wp-content/themes/StringLight2018/views/frontend/two_vertical_color_text_block.php
@@ -0,0 +1,8 @@
+<div class="grid-x grid-container module two-vertical-text">
+    <div class="cell small-22 medium-12 medium-offset-8">
+        <h2><?= $module['headline']; ?></h2>
+    </div>
+    <div class="cell small-22 medium-18 medium-offset-4">
+        <?= $module['content']; ?>
+    </div>
+</div>
